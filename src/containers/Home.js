1c40d1cc896b59e6a70ec13d6d71d81321004719
Justin Veach <justinwveach@gmail.com>
2018-02-06 13:06:40 -0600

    Content changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,18 +5,18 @@ import Header from './Header';
 class Home extends Component {
   render() {
       const airCentralTitle = 'DeSoto County\'s Best';
-      const airCentralText = "Since 2001, Air Central has been a premier choice for DeSoto County's heating & air conditioning needs.";
+      const airCentralText = "Since 2001, Air Central has been a premier choice for heating & air conditioning needs in DeSoto County and surrounding areas. We perform a wide variety of services ranging from yearly checkups to complete system replacements. We stand behind our work and offer 1 year warranty on all services!";
       const familyOwnedTitle = 'Family Owned & Operated';
-      const familyOwnedText = "Air Central is a family owned and operated business that takes pride in quality work.";
+      const familyOwnedText = "Air Central is a family owned and operated business that takes pride in quality work. We understand the importance of a home that is comfortable and welcoming, and we will ensure your home reaches its full potential.";
     return (
       <div className="Home">
-        <Header title="Air Central Heating & Cooling" tagline="DeSoto County's Premier Air Conditioning & Heating Service for over 20 years" headerPage="home" hideButton="" />
+        <Header title="Air Central Heating & Cooling" tagline="North Mississippi's Premier Air Conditioning & Heating Service for over 20 years" headerPage="home" hideButton="" />
         <div className="Home-mainContainer">
             <div className="Home-rowContainer flex-stack-sm">
                 <div className="Home-side">
                     <div className='Home-blurb Home-blurb-sm'>
-                        <h2>{airCentralTitle}</h2>
-                        {airCentralText}
+                        <h1>{airCentralTitle}</h1>
+                        <p>{airCentralText}</p>
                     </div>
                 </div>
                 <div className="Home-side Home-imgSide">
@@ -25,12 +25,12 @@ class Home extends Component {
             </div>
             <div className="Home-rowContainer flex-stack-sm">
                 <div className="Home-side Home-imgSide">
-                    <img className="Home-icon Home-left" src={require('../images/house.jpeg')}/>
+                    <img className="Home-icon Home-icon-sm Home-left" src={require('../images/house.jpeg')}/>
                 </div>
                 <div className="Home-side">
                     <div className="Home-blurb Home-blurb-sm">
-                        <h2>{familyOwnedTitle}</h2>
-                        {familyOwnedText}
+                        <h1>{familyOwnedTitle}</h1>
+                        <p>{familyOwnedText}</p>
                     </div>
                 </div>
             </div>
